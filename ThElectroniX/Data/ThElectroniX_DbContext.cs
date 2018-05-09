61abb9777558d1c2ba71a36e685fe0f039ae87fb
onchari wycliffe <onchariwycliffe@gmail.com>
2018-05-09 16:30:30 +0300

    ThElectroniX_001_WED

---
+++ ThElectroniX/Data/ThElectroniX_DbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThElectroniX.Models;
+
+namespace ThElectroniX.Data
+{
+    public class ThElectroniX_DbContext :DbContext
+    {
+        public ThElectroniX_DbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
